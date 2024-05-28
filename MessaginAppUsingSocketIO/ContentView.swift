ee93494f9de7a0b8ec4aa522c569cb021ac88274
Aniket Patil <aniket.p@reapmind.com>
2024-05-28 11:28:02 +0530

    Initial Commit

---
+++ MessaginAppUsingSocketIO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessaginAppUsingSocketIO
+//
+//  Created by Aniket Patil on 28/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
